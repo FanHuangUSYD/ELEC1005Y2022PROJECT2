81d03d10654ec648eddb845b7b901ad775feab33
Rui Yan <ryan6128@uni.sydney.edu.au>
2022-05-19 04:46:38 +1000

    testing first commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created on Wed May 16 15:22:20 2018
 @author: zou
 """
 
-from pickle import TRUE
+
 from tkinter import CENTER
 import pygame
 import time
@@ -18,10 +18,17 @@ black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 
 background = pygame.image.load('images/background.jpg')
-start_img = pygame.image.load('images/start_btn.png')
-start_img_ac = pygame.image.load('images/start_btnOn.png')
-exit_img = pygame.image.load('images/exit_btn.png')
-exit_img_ac = pygame.image.load('images/exit_btnOn.png')
+
+start_img_off = pygame.image.load('images/start_btn.png')
+start_img = pygame.image.load('images/start_btnOn.png')
+
+exit_img_off = pygame.image.load('images/exit_btn.png')
+exit_img = pygame.image.load('images/exit_btnOn.png')
+
+scroll_clsd_off = pygame.image.load('images/test1scroll_ac.png')
+scroll_clsd = pygame.image.load('images/test1scroll.png')
+
+scroll_opnd = pygame.image.load('images/scroll_opnd1.png')
 
 
 green = pygame.Color(0, 200, 0)
@@ -43,13 +50,22 @@ pygame.display.set_caption('Gluttonous')
 
 crash_sound = pygame.mixer.Sound('./sound/crash.wav')
 
-custom_font = pygame.font.Font("images/Wicked_Mouse.ttf", 35)
-custom_font_back = pygame.font.Font("images/Wicked_Mouse.ttf", 35)
-
-starting_message_back = custom_font_back.render("Gluttonous", TRUE, (0, 0, 0))
-starting_message = custom_font.render("Gluttonous", TRUE, (255, 191, 0))
+title_font = pygame.font.Font("images/Fonts/Wicked_Mouse.ttf", 35)
+instruct_font = pygame.font.Font('images/Fonts/Minecraft.ttf', 14)
 
 
+starting_message_back = title_font.render("Gluttonous", True, (0, 0, 0))
+starting_message = title_font.render("Gluttonous", True, (255, 191, 0))
+
+instruct_msg = instruct_font.render("Navigate around the", True, (106, 78, 66))
+instruct_msg1 = instruct_font.render("screen with", True, (106, 78, 66))
+instruct_msg2 = instruct_font.render("arrow keys.", True, (106, 78, 66))
+
+instruct_msg3 = instruct_font.render("Eat as many fruits", True, (106, 78, 66))
+instruct_msg4 = instruct_font.render("as possible and", True, (106, 78, 66))
+instruct_msg5 = instruct_font.render("avoid crashing.", True, (106, 78, 66))
+
+exit_instruction = pygame.USEREVENT + 0 #this creates a custom event, at the 24th ID as the first 23 are reserved
 
 def text_objects(text, font, color=black):
     text_surface = font.render(text, True, color)
@@ -63,12 +79,11 @@ def message_display(text, x, y, color=black):
     screen.blit(text_surf, text_rect)
     pygame.display.update()
 
-
 def button(msg, x, y, w, h, inactive_img, active_img, action=None, parameter=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
+
     if x + w > mouse[0] > x and y + h > mouse[1] > y: #checks if the current mouse coordinates collide with the buttons coordinates
-        pygame.draw.rect(screen, 0 ,(x, y, w, h))
         screen.blit(active_img, (x, y) )
         if click[0] == 1 and action != None:
             if parameter != None:
@@ -76,7 +91,6 @@ def button(msg, x, y, w, h, inactive_img, active_img, action=None, parameter=Non
             else:
                 action()
     else:
-        pygame.draw.rect(screen, 0, (x, y, w, h))
         screen.blit(inactive_img, (x, y))
 
     smallText = pygame.font.SysFont('comicsansms', 20)
@@ -84,35 +98,42 @@ def button(msg, x, y, w, h, inactive_img, active_img, action=None, parameter=Non
     TextRect.center = (x + (w / 2), y + (h / 2))
     screen.blit(TextSurf, TextRect)
 
-
 def quitgame():
     pygame.quit()
     quit()
-
+    
 
 def crash(): #when the player crashes to themselves, a sound plays and a defeat message displays
     pygame.mixer.Sound.play(crash_sound)
-    message_display('crashed', game.settings.width / 2 * 15, game.settings.height / 3 * 15, white)# can change to wasted here
+    message_display('crashed', 
+                    game.settings.width / 2 * 15,
+                    game.settings.height / 3 * 15, 
+                    white)# can change to wasted here
     time.sleep(1)
-
-
+        
 def initial_interface(): #menu
     intro = True
+    stop_loop = False
     while intro:
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+            if event.type == pygame.QUIT or intro == False:
+                quitgame()
 
         screen.fill(white) #starting screen display
         screen.blit(background, (0, 0))
-        screen.blit(starting_message_back, (game.settings.width / 5.9 * 15, game.settings.height / 4.6 * 15))
-        screen.blit(starting_message, (game.settings.width / 6.2 * 15, game.settings.height / 4.7 * 15))
 
-        #message_display('Gluttonous', game.settings.width / 2 * 15, game.settings.height / 4 * 15)
+        screen.blit(starting_message_back,
+                   (game.settings.width / 5.9 * 15, 
+                    game.settings.height / 4.6 * 15))
 
-        button('', 80, 210, 80, 40, start_img, start_img_ac, game_loop, 'human') #start button
-        button('', 240, 210, 80, 40, exit_img, exit_img_ac, quitgame) #exit button
+        screen.blit(starting_message,  
+                   (game.settings.width / 6.2 * 15, 
+                    game.settings.height / 4.7 * 15))
+
+        button('', 80, 185, 100, 45, start_img_off, start_img, game_loop, 'human') #start button
+        button('', 240, 185, 100, 45, exit_img_off, exit_img, quitgame) #exit button
+        button('', 165, 280, 90, 35, scroll_clsd_off, scroll_clsd, instruct_screen)
 
         pygame.display.update()
         pygame.time.Clock().tick(15)
@@ -143,6 +164,37 @@ def game_loop(player, fps=10):
 
     crash()
 
+def instruc_exit():
+    exit_instruction
+
+def instruct_screen():
+    instruct = True
+    stop_loop = False
+    while instruct:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+            elif event.type == exit_instruction:
+                instruct = False
+            
+        screen.fill(white)
+        screen.blit(background, (0, 0))
+
+        screen.blit(scroll_opnd, (85 , 50))
+
+        screen.blit(instruct_msg, (142, 117))
+        screen.blit(instruct_msg1, (170,137))
+        screen.blit(instruct_msg2, (170,157))
+
+        screen.blit(instruct_msg3, (148, 193))
+        screen.blit(instruct_msg4, (157,213))
+        screen.blit(instruct_msg5, (157,233))
+
+        button('', 160, 340, 100, 50, exit_img_off, exit_img, initial_interface)
+
+        pygame.display.update()
+        pygame.time.Clock().tick(15)
+           
 
 def human_move():
     direction = snake.facing #stores the current direction
